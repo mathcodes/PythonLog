ef8676f3dc180b11649e5d04efd040592c598d4e
fullstack11235 <fullstack11235@gmail.com>
2021-06-23 04:11:29 -0400

    "initial commit"

---
+++ other_blueprint.py
@@ -0,0 +1,10 @@
+# from flask.blueprints import Blueprint
+
+# other_blueprint = Blueprint('other', __name__)
+
+# from main import csrf
+
+# @csrf.exempt
+# @other_blueprint.route('/thing', methods=['GET', 'POST'])
+# def no_csrf_here():
+#     pass
